686323dba286932f6022e2994a3d3f8ed7867831
Mike <mikewrubel51@gmail.com>
2021-04-07 10:16:07 -0400

    commit tree 6e57daad5e6fecc8b5fb69851183474609fed2ac

--- index.js
+++ index.js
@@ -1,11 +1,51 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  
+  let respone;
+
+  if (distance <= 400){
+    respone = 'This one is on me!';
+  }
+  else if (distance > 2000 && distance <= 2500){
+    respone = 'I will gladly take your thirty bucks.';
+  }
+  else if (distance > 2500){
+    respone = 'No can do.';
+  }
+  return respone;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let answer;
+
+  if(city !== 'NYC'){
+    answer = 'No go.';
+  }
+  else {
+    answer = 'Ok, sounds good.';
+  }
+  return answer;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let goodBye;
+
+  switch (tip) {
+    case 'generous':
+      
+      goodBye = 'Thank you so much.';
+      break;
+
+      case 'not as generous':
+      
+      goodBye = 'Thank you.';
+      break;
+
+        default:
+          goodBye = 'Bye.';
+          break;
+  }
+  return goodBye;
 }
